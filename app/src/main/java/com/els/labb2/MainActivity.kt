ee2791d21612feab17d7d2e5d0d399904e2bad6d
Ella-Gotbring <54885982+Ella-Gotbring@users.noreply.github.com>
2022-04-26 16:58:20 +0200

    changed viewmodel

--- app/src/main/java/com/els/labb2/MainActivity.kt
+++ app/src/main/java/com/els/labb2/MainActivity.kt
@@ -11,33 +11,19 @@ import com.els.labb2.ViewModel.UserViewModel
 /* TODO READ THE README FILE FIRST */
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var userViewModel: UserViewModel
+
+    private lateinit var viewModel: UserViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         /** define and find with id  */
         val mainButton = findViewById<Button>(R.id.mainButton)
-        val addBtn = findViewById<Button>(R.id.addBtn)
-        val textNum = findViewById<TextView>(R.id.textView4)
         val intent = intent
         val email = intent.getStringExtra("Email")
         val password = intent.getStringExtra("Password")
 
-
-        /** connect to ViewModel and Instantiating
-         * added a button to increment value displayed by 1 when pressed
-         * */
-        userViewModel = ViewModelProvider(this)[UserViewModel::class.java]
-
-        addBtn.setOnClickListener{
-            userViewModel.addNumber()
-            textNum.text = userViewModel.number.toString()
-
-
-        }
-
-
+        viewModel = ViewModelProvider(this)[UserViewModel::class.java]
 
         /** added setOnClickListener that runs code if the button gets pressed*/
         /** define intent and what activity we want to change*/
@@ -53,7 +39,7 @@ class MainActivity : AppCompatActivity() {
 
         greeting.text = "Email: $email\nPassword: $password"
 
-
-
     }
 }
+
+
--- app/src/main/java/com/els/labb2/ViewModel/UserViewModel.kt
+++ app/src/main/java/com/els/labb2/ViewModel/UserViewModel.kt
@@ -2,16 +2,9 @@ package com.els.labb2.ViewModel
 
 import androidx.lifecycle.ViewModel
 
-/** created ViewModel package and moved file, added :ViewModel() and implemented the user info from login + a button */
+/** created ViewModel package and moved file, added :ViewModel()*/
 class UserViewModel : ViewModel(){
-    var email = ""
-    var password = ""
 
-    var number = 0
-
-    fun addNumber(){
-        number++
-    }
 
 
 
